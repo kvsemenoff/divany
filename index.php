94280833564575633db77fa782b4f40232392d66
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-19 14:28:45 +0300

    d

---
+++ index.php
@@ -0,0 +1,14 @@
+﻿<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header_top.php'); ?>
+<?php require_once('includes/portfolio.php'); ?>
+<?php require_once('includes/my_aproach.php'); ?>
+<?php require_once('includes/advantages.php'); ?>
+<?php require_once('includes/price.php'); ?>
+<?php require_once('includes/scheme.php'); ?>
+<?php require_once('includes/alsow.php'); ?>
+<?php require_once('includes/contacts.php'); ?>
+<?php require_once('includes/footer_bottom.php'); ?>
+<?php require_once('includes/footer.php'); ?>
+
+	
+
